90ebb2efc714c325ffaa948ec9f6c3c770f41f57
Nandan Joshi <nandan@gmx.de>
2021-02-27 23:06:05 +0100

    Added server listening, error handling and parsing.

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,8 @@
+use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -8,6 +13,28 @@ impl Server {
     }
 
     pub fn run(self) {
-        println!("Listening on {}", self.addr)
+        println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failer to parse a request: {}", e),
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }        
+        }
     }
 }
