f96da01e6a5cad893df0fc09125251a61f499310
Nandan Joshi <nandan@gmx.de>
2021-02-27 23:38:47 +0100

    Added query string parsing.

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,5 @@
 use super::method::{Method, MethodError};
+use super::{QueryString};
 use std::convert::TryFrom;
 use std::convert::From;
 use std::error::Error;
@@ -8,7 +9,7 @@ use std::str::Utf8Error;
 
 pub struct Request<'buf> {
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
     method: Method,
 }
 
@@ -69,7 +70,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
         //
         // Better way than using two statements above ...
         if let Some(i) = path.find('?') {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i]
         }
 
