6386db3ad5a8478485f818765c7bfb1ad9fdebc2
Nandan Joshi <nandan@gmx.de>
2021-02-28 20:54:33 +0100

    Added Getters and routing.

--- src/http/request.rs
+++ src/http/request.rs
@@ -14,6 +14,21 @@ pub struct Request<'buf> {
     method: Method,
 }
 
+impl<'buf> Request<'buf> {
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+
+    // note: Reference to the Option of a reference
+    pub fn query_string(&self) -> Option<&QueryString> {
+        self.query_string.as_ref()
+    }
+
+    pub fn method(&self) -> &Method {
+        &self.method
+    }
+}
+
 impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,17 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>Test</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome to Rust Test HTTP Server</h1>".to_string())),
+                "/say" => Response::new(StatusCode::Ok, Some("<h1>Welcome, my friend!</h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None),
+            }
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
