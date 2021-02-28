06388b9d2d835a73761f0c6c0d07f0c488ac2746
Nandan Joshi <nandan@gmx.de>
2021-02-28 21:32:07 +0100

    Added support for static files.

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,15 +1,43 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
+use std::fs;
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome to Rust Test HTTP Server</h1>".to_string())),
-                "/say" => Response::new(StatusCode::Ok, Some("<h1>Welcome, my friend!</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
+                "/say" => Response::new(StatusCode::Ok, self.read_file("say.html")),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             }
             _ => Response::new(StatusCode::NotFound, None),
         }
